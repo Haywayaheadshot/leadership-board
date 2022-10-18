67f7130656fe324f2cfca7c6ebd626dcdd80ef6f
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-10-18 17:14:46 +0000

    Add index.js file and write event listeners for submit and refresh

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+import { createScore, displayScores, refreshPage } from './modules/Functions.js';
+
+const submitScore = document.querySelector('.submit-score');
+const refreshButton = document.querySelector('.refresh-button');
+
+submitScore.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (document.getElementById('name').value === '' || document.getElementById('score').value === '') {
+    document.getElementsByClassName('error-message')[0].style.display = 'block';
+  } else {
+    document.getElementsByClassName('error-message')[0].style.display = 'none';
+    createScore();
+  }
+});
+
+window.onload = () => {
+  displayScores();
+};
+
+refreshButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  refreshPage();
+});
