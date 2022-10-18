9962d4956826d60309ce4375794aa7b594d439f0
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-10-18 17:15:19 +0000

    Create function and constructor modules

---
+++ src/modules/Constructor.js
@@ -0,0 +1,23 @@
+export default class AddScores {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  getScores = () => {
+    let dataStored = [];
+    let scores = [];
+    if (localStorage.getItem('scores')) {
+      dataStored = localStorage.getItem('scores');
+      // console.log(dataStored);
+      scores = JSON.parse(dataStored);
+    }
+    return scores;
+  }
+
+  addScore = () => {
+    const scores = this.getScores();
+    scores.push(this);
+    localStorage.setItem('scores', JSON.stringify(scores));
+  }
+}
