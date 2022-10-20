f01c19d5d8770fa4237e801b545ae5758954a156
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-20 15:02:02 -0700

    Post to api in createScore function and get api in displayScores function

--- src/modules/Functions.js
+++ src/modules/Functions.js
@@ -4,7 +4,7 @@ const scoreBoard = document.querySelector('.score-board-ul');
 const nameInput = document.querySelector('#name');
 const scoreInput = document.querySelector('#score');
 
-const createScore = () => {
+const createScore = async () => {
   const name = nameInput.value;
   const score = scoreInput.value;
   const newScoresInput = new AddScores(name, score);
@@ -16,21 +16,39 @@ const createScore = () => {
     <p class="score-board-name">${newScoresInput.name}:</p>
     <p class="score-board-score">${newScoresInput.score}</p>
     `;
+  // post new name and score to fetch API?
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1000/scores/', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    // make sure to serialize your JSON body
+    body: JSON.stringify({
+      user: nameInput.value,
+      score: scoreInput.value,
+    }),
+  })
+    .then((response) => response.json());
   nameInput.value = '';
   scoreInput.value = '';
   localStorage.getItem('scores');
 };
 
-const displayScores = () => {
-  const scores = new AddScores().getScores();
-  scores.forEach((score) => {
-    const newScoreLi = document.createElement('li');
-    newScoreLi.className = 'score-board-li';
-    scoreBoard.appendChild(newScoreLi);
-    newScoreLi.innerHTML = `
-            <p class="score-board-name">${score.name}:</p>
-            <p class="score-board-score">${score.score}</p>
-        `;
+const displayScores = async () => {
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1000/scores/').then((response) => response.json()).then((completeResponse) => {
+    const createdApiScores = completeResponse.result;
+    localStorage.setItem('scores', JSON.stringify(createdApiScores));
+    const scores = new AddScores().getScores();
+    scores.forEach((completeResponse) => {
+      const newScoreLi = document.createElement('li');
+      newScoreLi.className = 'score-board-li';
+      scoreBoard.appendChild(newScoreLi);
+      newScoreLi.innerHTML = `
+              <p class="score-board-name">${completeResponse.user}:</p>
+              <p class="score-board-score">${completeResponse.score}</p>
+          `;
+    });
   });
 };
 
