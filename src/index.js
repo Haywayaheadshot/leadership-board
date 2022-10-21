02acfbfcef4c0aa51980bbcb67668813474a3fc0
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-21 14:42:16 -0700

    Add event listener for enter key

--- src/index.js
+++ src/index.js
@@ -15,6 +15,18 @@ submitScore.addEventListener('click', (event) => {
   }
 });
 
+document.addEventListener('keypress', (event) => {
+  if (event.keyCode === 13 || event.which === 13) {
+    event.preventDefault();
+    if (document.getElementById('name').value === '' || document.getElementById('score').value === '') {
+      document.getElementsByClassName('error-message')[0].style.display = 'block';
+    } else {
+      document.getElementsByClassName('error-message')[0].style.display = 'none';
+      createScore();
+    }
+  }
+});
+
 window.onload = () => {
   displayScores();
 };
