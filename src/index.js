9eba45694ccd1dede7056c11f3d013a87e65b22f
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-05 08:25:30 +0000

    Build checkInput function to handle empty values and values with length longer than 10 characters

--- src/index.js
+++ src/index.js
@@ -2,29 +2,38 @@ import './style.css';
 
 import { createScore, displayScores, refreshPage } from './modules/Functions.js';
 
-const submitScore = document.querySelector('.submit-score');
+const submitScore = document.querySelector('#submit-score-id');
 const refreshButton = document.querySelector('.refresh-button');
 const errorMessage = document.querySelector('.error-message');
+const lengthErrorMessage = document.querySelector('.length-error-message');
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
 
-submitScore.addEventListener('click', (event) => {
-  event.preventDefault();
-  if (document.getElementById('name').value === '' || document.getElementById('score').value === '') {
+const checkInput = (event) => {
+  if (nameInput.value === '' || scoreInput.value === '') {
+    event.preventDefault();
     errorMessage.style.display = 'block';
+    lengthErrorMessage.style.display = 'none';
+    return;
+  } 
+  if (nameInput.value.length > 10 || scoreInput.value.length > 10) {
+    event.preventDefault();
+    errorMessage.style.display = 'none';
+    lengthErrorMessage.style.display = 'block';
   } else {
     errorMessage.style.display = 'none';
+    lengthErrorMessage.style.display = 'none';
     createScore();
   }
+};
+
+submitScore.addEventListener('click', (event) => {
+  checkInput(event);
 });
 
 document.addEventListener('keypress', (event) => {
   if (event.keyCode === 13 || event.which === 13) {
-    event.preventDefault();
-    if (document.getElementById('name').value === '' || document.getElementById('score').value === '') {
-      errorMessage.style.display = 'block';
-    } else {
-      errorMessage.style.display = 'none';
-      createScore();
-    }
+    checkInput(event);
   }
 });
 
